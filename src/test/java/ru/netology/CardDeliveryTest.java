48fdf1841c2260ae05f71c85ef9126f94258a7c3
vaysamc <vasak7503@gmail.com>
2024-06-23 17:36:52 +0300

    Selen commit

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,73 +1,55 @@
 package ru.netology;
 
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    private WebDriver driver;
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // запуск фон. режим браузер
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // Открыть браузер и перейти на страницу приложения
+        open("http://localhost:9999");
     }
 
     @AfterEach
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        // Закрыть браузер после выполнения тестов
+        closeWebDriver();
     }
 
     @Test
     public void testCardDeliveryForm() {
-        driver.get("http://localhost:9999");
+        // Заполнение поля "Город"
+        $("[data-test-id=city] input").setValue("Москва");
 
-        // поле город
-        WebElement cityField = driver.findElement(By.cssSelector("[data-test-id=city] input"));
-        cityField.sendKeys("Москва");
-
-        // поле дата
-        WebElement dateField = driver.findElement(By.cssSelector("[data-test-id=date] input"));
-        dateField.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE); // Очистка поля
+        // Заполнение поля "Дата"
         LocalDate date = LocalDate.now().plusDays(3);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        dateField.sendKeys(date.format(formatter));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(date.format(formatter));
 
-        // поля фамилия и имя
-        WebElement nameField = driver.findElement(By.cssSelector("[data-test-id=name] input"));
-        nameField.sendKeys("Иванов Иван");
+        // Заполнение поля "Фамилия и Имя"
+        $("[data-test-id=name] input").setValue("Иванов Иван");
 
-        // поле телефон
-        WebElement phoneField = driver.findElement(By.cssSelector("[data-test-id=phone] input"));
-        phoneField.sendKeys("+79991234567");
+        // Заполнение поля "Телефон"
+        $("[data-test-id=phone] input").setValue("+79991234567");
 
-        // флажок согласия
-        WebElement agreementCheckbox = driver.findElement(By.cssSelector("[data-test-id=agreement]"));
-        agreementCheckbox.click();
+        // Установка флажка согласия
+        $("[data-test-id=agreement]").click();
 
-        // отправка формы
-        WebElement submitButton = driver.findElement(By.cssSelector("[data-test-id=submit]"));
-        submitButton.click();
+        // Отправка формы
+        $("[data-test-id=submit]").click();
 
-        // проверка
-        WebElement successNotification = driver.findElement(By.cssSelector("[data-test-id=success-notification]"));
-        assertTrue(successNotification.isDisplayed(), "Уведомление об успешной отправке не отображается");
+        // Проверка успешной отправки
+        $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 }
