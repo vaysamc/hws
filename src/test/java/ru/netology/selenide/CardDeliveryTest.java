ed731a8ece80b71cb69daa4023a20c3e9a0ee700
vaysamc <vasak7503@gmail.com>
2024-07-01 21:42:49 +0300

    text commit

--- src/test/java/ru/netology/selenide/CardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -8,8 +8,8 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
@@ -21,14 +21,14 @@ public class CardDeliveryTest {
     public void testCardDeliveryForm() {
         open ( "http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(5, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        String planningDate = generateDate(5, "dd.MM. yvyy");
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иванов-Иваныч Иван");
         $("[data-test-id='phone'] input").setValue("+78083253945");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $("_notification__content").shouldBe(Condition.visible, Duration.ofSeconds(30))
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
